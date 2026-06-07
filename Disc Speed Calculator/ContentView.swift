5fe60bac2bc02961d23cc7a1d0856410b6cf9461
Stian Holtet <imtaisen@gmail.com>
2026-06-07 04:54:07 +0200

    oppdaterte flowchart

--- Disc Speed Calculator/ContentView.swift
+++ Disc Speed Calculator/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background-wood-grain")
+            Text("Velkommen til min app")
+                .font(Font.largeTitle.bold())
         }
-        .padding()
     }
 }
 
