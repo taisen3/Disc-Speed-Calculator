522eb107517a9b7703badcb61de6353bc906feb7
Stian Holtet <imtaisen@gmail.com>
2026-06-07 03:44:52 +0200

    Initial Commit

---
+++ Disc Speed Calculator/Disc_Speed_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Disc_Speed_CalculatorApp.swift
+//  Disc Speed Calculator
+//
+//  Created by Stian Holtet on 07/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Disc_Speed_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
